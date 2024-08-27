0fec7ad905858a30c41571fb0ba99c2168a44b17
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2024-08-27 02:13:13 -0700

    update url backend to work with frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://www.botbotphotography.com'
 }))
 app.use(express.json());
 
